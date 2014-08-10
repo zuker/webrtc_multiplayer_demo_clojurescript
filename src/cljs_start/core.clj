81800fc7bbba5aaca8ba6d4e8c6107a85937e230
zuker <lndkuzmin@gmail.com>
2014-08-10 12:04:36 +0300

    project created

---
+++ src/cljs_start/core.clj
@@ -0,0 +1,1 @@
+( ns cljs-start.core )
---
+++ test/cljs_start/test/core.clj
@@ -0,0 +1,6 @@
+( ns cljs-start.test.core
+( : use [ cljs-start.core ] )
+( : use [ clojure.test ] ) )
+
+( deftest replace-me  ;; FIXME: write
+ ( is false "No tests have been written." ) )
